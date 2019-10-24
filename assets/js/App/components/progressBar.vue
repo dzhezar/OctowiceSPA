5f6532e5cae52c25267a7f9fc100da7f6ee61d11
dzhezar <dzhezik@gmail.com>
2019-10-24 20:26:14 +0300

    Fixed section styles and progress bar added

---
+++ assets/js/App/components/progressBar.vue
@@ -0,0 +1,3 @@
+<template>
+    <VueScrollProgress></VueScrollProgress>
+</template>
--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,12 @@ import VueRouter from "vue-router";
 import router from "./App/router";
 import App from "./App/App";
 let VueScrollTo = require('vue-scrollto');
+import VueScrollProgress from 'vue-scroll-progress';
 import 'axios';
 Vue.use(VueRouter);
 Vue.use(VueScrollTo);
+Vue.use(VueScrollProgress);
+
 Vue.component('country-flag', CountryFlag);
 import { i18n } from './i18n'
 
